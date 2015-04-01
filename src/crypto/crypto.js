07b18ed4eee26e8b3021affe15b4e588dd4ae43f
Arturo Filastò <art@fuffa.org>
2015-04-01 19:29:01 +0200

    Fix crypto

--- src/crypto/crypto.js
+++ src/crypto/crypto.js
@@ -181,62 +181,66 @@ module.exports = {
   },
   
   customRSA: function(bits, prng) {
-    var B = bits;
-    var E = "10001";
-    var rsa = new publicKey.rsa();
-    var key = new rsa.keyObject();
-    var rng;
-    if (prng) {
-      rng = prng;
-    } else {
-      rng = new rsa.SecureRandom();
-    }
-    var qs = B >> 1;
-    key.e = parseInt(E, 16);
-    key.ee = new BigInteger(E, 16);
-    for (;;) {
+    return new Promise(function(resolve) {
+      var B = bits;
+      var E = "10001";
+      var rsa = new publicKey.rsa();
+      var key = new rsa.keyObject();
+      var rng;
+      if (prng) {
+        rng = prng;
+      } else {
+        rng = new rsa.SecureRandom();
+      }
+      var qs = B >> 1;
+      key.e = parseInt(E, 16);
+      key.ee = new BigInteger(E, 16);
       for (;;) {
-        key.p = new BigInteger(B - qs, 1, rng);
-        if (key.p.subtract(BigInteger.ONE).gcd(key.ee).compareTo(BigInteger.ONE) === 0 && key.p.isProbablePrime(10))
+        for (;;) {
+          key.p = new BigInteger(B - qs, 1, rng);
+          if (key.p.subtract(BigInteger.ONE).gcd(key.ee).compareTo(BigInteger.ONE) === 0 && key.p.isProbablePrime(10))
+            break;
+        }
+        for (;;) {
+          key.q = new BigInteger(qs, 1, rng);
+          if (key.q.subtract(BigInteger.ONE).gcd(key.ee).compareTo(BigInteger.ONE) === 0 && key.q.isProbablePrime(10))
+            break;
+        }
+        if (key.p.compareTo(key.q) <= 0) {
+          var t = key.p;
+          key.p = key.q;
+          key.q = t;
+        }
+        var p1 = key.p.subtract(BigInteger.ONE);
+        var q1 = key.q.subtract(BigInteger.ONE);
+        var phi = p1.multiply(q1);
+        if (phi.gcd(key.ee).compareTo(BigInteger.ONE) === 0) {
+          key.n = key.p.multiply(key.q);
+          key.d = key.ee.modInverse(phi);
+          key.dmp1 = key.d.mod(p1);
+          key.dmq1 = key.d.mod(q1);
+          key.u = key.p.modInverse(key.q);
           break;
+        }
       }
-      for (;;) {
-        key.q = new BigInteger(qs, 1, rng);
-        if (key.q.subtract(BigInteger.ONE).gcd(key.ee).compareTo(BigInteger.ONE) === 0 && key.q.isProbablePrime(10))
-          break;
+      resolve(key);
+    }).then(function(key) {
+      function mapResult(result) {
+        return result.map(function(bn) {
+          var mpi = new type_mpi();
+          mpi.fromBigInteger(bn);
+          return mpi;
+        });
       }
-      if (key.p.compareTo(key.q) <= 0) {
-        var t = key.p;
-        key.p = key.q;
-        key.q = t;
-      }
-      var p1 = key.p.subtract(BigInteger.ONE);
-      var q1 = key.q.subtract(BigInteger.ONE);
-      var phi = p1.multiply(q1);
-      if (phi.gcd(key.ee).compareTo(BigInteger.ONE) === 0) {
-        key.n = key.p.multiply(key.q);
-        key.d = key.ee.modInverse(phi);
-        key.dmp1 = key.d.mod(p1);
-        key.dmq1 = key.d.mod(q1);
-        key.u = key.p.modInverse(key.q);
-        break;
-      }
-    }
-    function mapResult(result) {
-      return result.map(function(bn) {
-        var mpi = new type_mpi();
-        mpi.fromBigInteger(bn);
-        return mpi;
-      });
-    }
-    var output = [];
-    output.push(key.n);
-    output.push(key.ee);
-    output.push(key.d);
-    output.push(key.p);
-    output.push(key.q);
-    output.push(key.u);
-    return mapResult(output);
+      var output = [];
+      output.push(key.n);
+      output.push(key.ee);
+      output.push(key.d);
+      output.push(key.p);
+      output.push(key.q);
+      output.push(key.u);
+      return mapResult(output);
+    });
   },
 
   generateMpi: function(algo, bits) {
